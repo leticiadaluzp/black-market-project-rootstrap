e2d03e6be5635c6130d41435348cf6b96ff67d2f
Diana Castaño <dianaczapata@gmail.com>
2024-07-09 14:41:59 -0500

    add Header component

---
+++ src/component/Header/index.jsx
@@ -0,0 +1,18 @@
+import './index.css'
+
+export default function Header() {
+ return (
+  <>
+  <header>
+   <div className='logo-container'>
+    <img src="" alt="logo" />
+    <h1>Black Market</h1>
+   </div>
+   <div className='buttons-container'>
+    <button>Sign In</button>
+    <button>Sign Up</button>
+   </div>
+  </header>
+  </>
+ )
+}
