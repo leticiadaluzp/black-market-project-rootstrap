e84c5409d8dab83c20635f68b32f489fdbe03f78
Diana Castaño <dianaczapata@gmail.com>
2024-07-14 23:39:40 -0500

    update header style and add logo

--- src/component/Header/index.jsx
+++ src/component/Header/index.jsx
@@ -1,16 +1,17 @@
 import './index.css'
+import logo from '../../assets/logo.png'
 
 export default function Header() {
  return (
   <>
   <header>
    <div className='logo-container'>
-    <img src="" alt="logo" />
+    <img src={logo} alt="logo" />
     <h1>Black Market</h1>
    </div>
    <div className='buttons-container'>
-    <button>Sign In</button>
-    <button>Sign Up</button>
+    <button className='sign-button'>Sign In</button>
+    <button className='sign-button'>Sign Up</button>
    </div>
   </header>
   </>
