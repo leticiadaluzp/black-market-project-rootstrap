a172f83c16aaf70bfe7af1d9b945668fb496444a
leticiadaluzp <leticia.daluz.p@gmail.com>
2024-08-17 16:30:38 -0300

    API integration for user registration

--- src/component/Input/index.jsx
+++ src/component/Input/index.jsx
@@ -1,8 +1,8 @@
-export function Input({id, name, placeholder, type}){
+export function Input({id, name, placeholder, type, value, onChange}){
   return (
     <div className='flex flex-col gap-1 w-11/12 m-auto mb-5 md:max-w-1/2'>
       <label htmlFor={id} className="md:pl-20">{name}</label>
-      <input type={type} name={id} id={id} required className='rounded-3xl h-8 placeholder:pl-2  indent-3 text-gray-700 md:m-auto md:w-[400px]' placeholder={placeholder} />
+      <input type={type} name={id} id={id} value={value} onChange={onChange} required className='rounded-3xl h-8 placeholder:pl-2  indent-3 text-gray-700 md:m-auto md:w-[400px]' placeholder={placeholder} />
     </div>
   )
 }
