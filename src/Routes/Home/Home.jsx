785c745240b1962adc9f5754f713126d5b37b3ea
leticiadaluzp <leticia.daluz.p@gmail.com>
2024-08-18 16:24:11 -0300

    API integration for User login and homepage redesign

--- src/Routes/Home/Home.jsx
+++ src/Routes/Home/Home.jsx
@@ -1,13 +1,78 @@
+import React, { useState, useEffect } from 'react';
+import {axiosInstance} from "../../component/axios/axios";;
 import creepyClerk from '../../assets/Igor_Persona5.gif'
 
 export function Home(){
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const authStatus = checkAuth(); 
+    setIsAuthenticated(authStatus);
+    console.log("en el useEffect");
+    if (authStatus) {
+      console.log("en el if")
+      fetchProducts();
+    }
+  }, []);
+
+  const checkAuth = () => {
+    const storedAccessToken = localStorage.getItem('accessToken');
+    const storedUid = localStorage.getItem('uid');
+    const storedClient = localStorage.getItem('client');
+  
+    if (storedAccessToken && storedUid && storedClient) {
+      return true; 
+    }
+    return false;  
+  }
+
+  const fetchProducts = async () => {
+    try {
+      const response = await axiosInstance.get("/products", {
+        headers: {
+          'access-token': localStorage.getItem('access-token'),
+          'uid': localStorage.getItem('uid'),
+          'client': localStorage.getItem('client'),
+        },
+      });
+      setProducts(response.data.data);
+    } catch (error) {
+      console.error("Failed to fetch products:", error);
+    }
+  };
+
   return(
-    <div className='flex flex-col items-center mt-8 md:mt-0'>
-      <h1 className='text-2xl mt-3 md:mt-10 md:text-4xl'>Welcome to...</h1>
-      <div className='w-full h-full flex items-center relative text-center overflow-hidden'>
-        <h2 className='relative text-[4rem] font-bold m-auto leading-normal tracking-wider z-10 text-glitch text-glitch-duration-fast md:text-[4.4rem]'>Black Market</h2>
-      </div>
-      <img src={creepyClerk} alt="Igor persona character" className='w-11/12 mt-10 md:w-1/2 md:mt-8' />
-    </div>
-  )
+    <>
+      {isAuthenticated ? (
+         <section>
+            <h1 className='text-2xl mt-3 md:mt-10 md:text-4xl text-center mb-6'>Explore Our Products</h1>
+            <div>
+              <ul className='flex flex-wrap justify-center'>
+                {products.map(({ title, pictures, description, unit_price }) => (
+                  <li key={title} className='m-4 w-64'>
+                    <div className="bg-white rounded-lg shadow-md overflow-hidden h-full flex flex-col">
+                      <img className='h-48 w-full object-contain p-2' src={pictures[0]} alt={title} />
+                      <div className='p-4 flex-grow'>
+                        <h3 className='font-bold text-lg text-gray-800 mb-2'>{title}</h3>
+                        <p className='text-gray-500 text-sm'>{description}</p>
+                        <span className='block mt-2 font-semibold text-xl text-black'>{unit_price}</span>
+                      </div>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </section>
+      ) : (
+        <div className='flex flex-col items-center mt-8 md:mt-0'>
+          <h1 className='text-2xl mt-3 md:mt-10 md:text-4xl'>Welcome to...</h1>
+          <div className='w-full h-full flex items-center relative text-center overflow-hidden'>
+            <h2 className='relative text-[4rem] font-bold m-auto leading-normal tracking-wider z-10 text-glitch text-glitch-duration-fast md:text-[4.4rem]'>Black Market</h2>
+          </div>
+          <img src={creepyClerk} alt="Igor persona character" className='w-11/12 mt-10 md:w-1/2 md:mt-8' />
+        </div>
+      )}
+    </>
+  );
 }
--- src/Routes/Sign-In/Sign-in.jsx
+++ src/Routes/Sign-In/Sign-in.jsx
@@ -1,12 +1,58 @@
-import { Input } from "../../component"
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import { Input, Loader } from "../../component";
+import { axiosInstance } from "../../component/axios/axios";
 
 export function SignIn (){
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(""); 
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+
+    try {
+      const response = await axiosInstance.post("/users/sign_in", {
+        user: {
+          email,
+          password,
+        }
+      });
+      console.log("User login successful:", response);
+      const accessToken = response.headers['access-token'];
+      const uid = response.headers['uid'];
+      const client = response.headers['client'];
+      
+      localStorage.setItem('accessToken', accessToken);
+      localStorage.setItem('uid', uid);
+      localStorage.setItem('client', client);
+
+      navigate("/");
+    } catch (error){
+        if (error.response && error.response.status === 401) {
+          setError(error.response.data.error);
+        } else {
+          setError("An unexpected error occurred.");
+        }
+    } finally {
+        setIsLoading(false);
+        setEmail("");
+        setPassword("");
+    }
+  };
+
+  if (isLoading) return <Loader />
+
   return (
     <section className='mt-8 m-auto md:max-w-[600px] md:mt-14 md:pb-5 md:rounded-3xl md:border md:border-solid md:border-slate-50  '>
       <h2 className='text-center md:mt-5 md:text-xl'>Sign In </h2>
-      <form action='' className='flex flex-col'>
-        <Input name='Email' id='email' type='email' placeholder='Write your email here!'/>
-        <Input name='Password' id='password' type='password' placeholder='Write your password here!'/>
+      <form onSubmit={handleSubmit} action='' className='flex flex-col'>
+        <Input name='Email' id='email' type='email' value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Write your email here!'/>
+        <Input name='Password' id='password' type='password' value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Write your password here!'/>
+        {error && <p className="text-red-500 flex items-center w-11/12 m-auto md:pl-20">{error}</p>}       
         <input type="submit" value="Sign in" className='m-auto w-32 h-10 border border-slate-50 border-solid rounded-lg transition-all ease-in-out duration-100 md:text-lg md:hover:bg-red-800' />
       </form>
       <div className='flex flex-col items-center gap-1 mt-6'>
