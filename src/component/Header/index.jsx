510e78a878610d7e144e5145c971b89305539e5b
leticiadaluzp <leticia.daluz.p@gmail.com>
2024-08-27 17:45:01 -0300

    fix header logo link

--- src/component/Header/index.jsx
+++ src/component/Header/index.jsx
@@ -62,7 +62,7 @@ export function Header() {
       <ToastContainer />
       <header className='flex box-border justify-between w-full h-10 py-0.5 px-2.5 md:h-16 md:py-0 md:px-[10%] md:shadow-extra'>
         <div>
-          <Link to={'/'} className='flex items-center pl-0.25 gap-1 pt-2 md:pt-4'>
+          <Link to={isLoggedIn ? 'homepage' : '/'} className='flex items-center pl-0.25 gap-1 pt-2 md:pt-4'>
             <img src={logo} alt="logo" className='w-8 h-8' />
             <h1 className='font-logo text-sm md:text-lg'>Black Market</h1>
           </Link>
