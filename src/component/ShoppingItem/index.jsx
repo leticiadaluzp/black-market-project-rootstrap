0faf3ee669aae41e1eed36c4ae84eea546b0a571
Diana Castaño <dianaczapata@gmail.com>
2024-08-29 01:01:13 -0500

    fix space between tags

--- src/component/ShoppingItem/index.jsx
+++ src/component/ShoppingItem/index.jsx
@@ -4,14 +4,14 @@ import { FaTrashCan } from 'react-icons/fa6'
 export function ShoppingItem ({name, quantity, price, picture, onIncrement, onDecrement}){
  return(
   <li className='flex flex-col md:flex-row gap-4 items-center md:justify-around border-2 border-slate-200 rounded-lg py-4 md:py-2 w-full md:px-6 mt-4'>
-   <img src={picture} alt="product" className='w-12 h-12 rounded' />
-   <h2>{name}</h2>
+   <img src={picture} alt="product" className='md:w-12 h-12 rounded' />
+   <h2 className='w-10'>{name}</h2>
    <div className='flex gap-8'>
-    <div className='flex flex-col items-center'>
+    <div className='flex flex-col items-center md:w-12'>
      <h2>Quantity</h2>
      <p>{quantity}</p>
     </div>
-    <div className='flex flex-col items-center'>
+    <div className='flex flex-col items-center md:w-24'>
      <h2>Unit price</h2>
      <p>{price}</p>
     </div>
