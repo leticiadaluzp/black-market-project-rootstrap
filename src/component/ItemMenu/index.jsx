d1845003bf6bad2da1da47c1b63b72a6c57b12d9
Diana Castaño <dianaczapata@gmail.com>
2024-08-29 00:42:53 -0500

    fix typo

--- src/component/ItemMenu/index.jsx
+++ src/component/ItemMenu/index.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 export function ItemMenu({text, path, onClick}){
   return (
-    <li className='h-full border-non px-1 ease-in-out duration-200 pt-3.5 md:pt-5 text-sm md:text-lg md:px-4 md:hover:bg-red-800'>
+    <li className='h-full border-none px-1 ease-in-out duration-200 pt-3.5 md:pt-5 text-sm md:text-lg md:px-4 md:hover:bg-red-800'>
       <Link to={path} onClick={onClick}>
         {text}
       </Link>
