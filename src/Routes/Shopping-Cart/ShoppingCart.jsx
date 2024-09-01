9baa5b3ee538b624f4a62c2e3ef5e8d647947f2a
leticiadaluzp <leticia.daluz.p@gmail.com>
2024-09-01 14:51:27 -0300

    add API requests

--- src/Routes/Shopping-Cart/ShoppingCart.jsx
+++ src/Routes/Shopping-Cart/ShoppingCart.jsx
@@ -1,5 +1,7 @@
 import { useContext, useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
+import "react-toastify/dist/ReactToastify.css";
+import { toast } from 'react-toastify';
 import { UserSessionContext } from '../../component/context/UserSessionProvider'
 import {SearchInput, Loader, SortButton, ShoppingItem} from '../../component'
 import {axiosInstance} from '../../component/axios/axios'
@@ -8,57 +10,56 @@ export function ShoppingCart(){
   const [isAuthenticated, setIsAuthenticated] = useState(false)
   const { isLoggedIn } = useContext(UserSessionContext)
   const [isLoading, setIsLoading] = useState(true)
- const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
   const navigate = useNavigate()
 
- useEffect(() => {
-      handleAuthentication()
+  useEffect(() => {
+    handleAuthentication()
     }, [])
-
-
-     const handleAuthentication = () => {
-      setIsLoading(true)
-      const authStatus = checkAuth()
-      setIsAuthenticated(authStatus)
-      if (!authStatus) {
-        navigate('/sign-in')
-      } else {
-        fetchProducts()
-      }
+  
+  const handleAuthentication = () => {
+    setIsLoading(true)
+    const authStatus = checkAuth()
+    setIsAuthenticated(authStatus)
+    if (!authStatus) {
+      navigate('/sign-in')
+    } else {
+      fetchProducts()
     }
+  }
 
-    const checkAuth = () => {
-      const storedAccessToken = localStorage.getItem('accessToken')
-      const storedUid = localStorage.getItem('uid')
-      const storedClient = localStorage.getItem('client')
+  const checkAuth = () => {
+    const storedAccessToken = localStorage.getItem('accessToken')
+    const storedUid = localStorage.getItem('uid')
+    const storedClient = localStorage.getItem('client')
 
-      if (storedAccessToken && storedUid && storedClient) {
-        return true;
-      }
-      return false;
+    if (storedAccessToken && storedUid && storedClient) {
+      return true;
     }
+    return false;
+  }
 
-    const fetchProducts = async () => {
-      try {
-        const response = await axiosInstance.get('/shopping_cart', {
-          headers: {
-            'access-token': localStorage.getItem('accessToken'),
-            'uid': localStorage.getItem('uid'),
-            'client': localStorage.getItem('client'),
-          },
-        });
-        if (response && response.data && response.data.line_items){
-          setProducts(response.data.line_items);
-        }else{
-          throw new Error ('Oops, an error occured. Sorry about the inconvenience, please try refreshing and contact us if the problem persists')
-        }
-        console.log(response.data)
-      } catch (error) {
-        console.error('Failed to fetch products:', error)
-      } finally {
-        setIsLoading(false)
+  const fetchProducts = async () => {
+    try {
+      const response = await axiosInstance.get('/shopping_cart', {
+        headers: {
+          'access-token': localStorage.getItem('accessToken'),
+          'uid': localStorage.getItem('uid'),
+          'client': localStorage.getItem('client'),
+        },
+      });
+      if (response && response.data && response.data.line_items){
+        setProducts(response.data.line_items);
+      }else{
+        throw new Error ('Oops, an error occured. Sorry about the inconvenience, please try refreshing and contact us if the problem persists')
       }
+      console.log(response.data)
+    } catch (error) {
+      console.error('Failed to fetch products:', error)
+    } finally {
+      setIsLoading(false)
     }
+  }
 
   const handleSearch = (query) => {
     const searchTerms = query.split(',').map(term => term.trim().toLowerCase())
@@ -106,19 +107,43 @@ export function ShoppingCart(){
     setProducts(sortedProducts);
   }
 
- const incrementQuantity = (index) => {
+  const incrementQuantity = (index) => {
     const updatedProducts = [...products];
-    updatedProducts[index].quantity += 1;
-    setProducts(updatedProducts);
+    const productStock = updatedProducts[index].product.stock;
+
+    if (updatedProducts[index].quantity < productStock) {
+      updatedProducts[index].quantity += 1;
+      setProducts(updatedProducts);
+    } else {
+      toast.error('Insufficient stock available.');
+    }
   };
 
- const decrementQuantity = (index) => {
+  const decrementQuantity = (index) => {
     const updatedProducts = [...products];
     if (updatedProducts[index].quantity > 1) {
       updatedProducts[index].quantity -= 1;
     }
     setProducts(updatedProducts);
   };
+
+  const handleDelete = async (productId) => {
+    console.log("id:" , productId)
+    try {
+      await axiosInstance.delete(`/shopping_cart/line_items/${productId}`, {
+        headers: {
+          'access-token': localStorage.getItem('accessToken'),
+          'uid': localStorage.getItem('uid'),
+          'client': localStorage.getItem('client'),
+        },
+      });
+      setProducts(prevProducts => prevProducts.filter(product => product.product.id !== productId));
+      console.log("prod dsp de eliminar" , products)
+      window.location.reload();
+    } catch (error) {
+      toast.error('Failed to delete product.')
+    }
+  }
 
   const calculateTotalPrice = () => {
     const totalPrice = products.reduce((accumulator, product) => {
@@ -135,41 +160,49 @@ export function ShoppingCart(){
   const totalPurchasePrice = calculateTotalPrice()
 
 
-  if (isLoading || !products) return <Loader />
-  //Ajustar estilos y demas
-  if (!products || products.length === 0) return <p>No hay productos en el carrito.</p>;
+  if (isLoading || !products) return <Loader /> 
 
  return(
   <section className='mt-8 m-auto md:max-w-[800px] md:mt-14 md:pb-5 md:rounded-3xl md:border md:border-solid md:border-slate-50 flex flex-col items-center '>
-   < SearchInput onSearch={handleSearch} />
-   <div className='flex gap-5 mt-4 items-center justify-center flex-wrap px-3'>
-    <h2 className='w-full text-center text-base md:text-xl'>Sort by:</h2>
-    <SortButton text='Higher price' onClick={handleSortByPriceDescAndAZ} />
-    <SortButton text='Lower price' onClick={handleSortByPriceAscAndAZ}  />
-    <SortButton text='A-Z' onClick={handleSortAZ} />
-    <SortButton text='Z-A' onClick={handleSortZA}  />
-   </div>
-   <ul className='flex flex-col items-center mt-4 w-5/6'>
-      {products.map((product, index) => {
-        const productData = product.product || {}; // Acceso a la propiedad `product`
-        const picture = (productData.pictures && productData.pictures.length > 0) 
-          ? productData.pictures[0] 
-          : 'default-image.png';
+    < SearchInput onSearch={handleSearch} />
+    <div className='flex gap-5 mt-4 items-center justify-center flex-wrap px-3'>
+      <h2 className='w-full text-center text-base md:text-xl'>Sort by:</h2>
+      <SortButton text='Higher price' onClick={handleSortByPriceDescAndAZ} />
+      <SortButton text='Lower price' onClick={handleSortByPriceAscAndAZ}  />
+      <SortButton text='A-Z' onClick={handleSortAZ} />
+      <SortButton text='Z-A' onClick={handleSortZA}  />
+    </div>
+   {products.length === 0 ? (
+      <>
+        <p className='mt-10 p-3 text-center text-4xl text-red-600'>Your shopping cart is empty</p>
+        <p className=' text-center text-s '>There are not products to show</p>
+      </>
+    ) : (
+      <>
+        <ul className='flex flex-col items-center mt-4 w-5/6'>
+          {products.map((product, index) => {
+            const productData = product.product || {}; // Acceso a la propiedad `product`
+            const picture = (productData.pictures && productData.pictures.length > 0) 
+              ? productData.pictures[0] 
+              : 'default-image.png';
 
-        return (
-          <ShoppingItem
-            key={index}
-            name={productData.title || 'Unnamed product'}
-            quantity={product.quantity || 0}
-            price={productData.unit_price || 'Price not available'}
-            picture={picture}
-            onIncrement={() => incrementQuantity(index)}
-            onDecrement={() => decrementQuantity(index)}
-          />
-        );
-      })}
-   </ul>
-   <h3 className='mt-8 text-center text-xl mb-8 p-3 border border-gray-300 shadow-md rounded-md'>Total price: ${totalPurchasePrice} </h3>
+            return (
+              <ShoppingItem
+                key={index}
+                name={productData.title || 'Unnamed product'}
+                quantity={product.quantity || 0}
+                price={productData.unit_price || 'Price not available'}
+                picture={picture}
+                onIncrement={() => incrementQuantity(index)}
+                onDecrement={() => decrementQuantity(index)}
+                onDelete={() => handleDelete(product.id)}
+              />
+            );
+          })}
+        </ul>
+        <h3 className='mt-8 text-center text-xl mb-8 p-3 border border-gray-300 shadow-md rounded-md'>Total price: ${totalPurchasePrice} </h3>
+      </>
+    )}
   </section>
  )
 }
--- src/component/ShoppingItem/index.jsx
+++ src/component/ShoppingItem/index.jsx
@@ -1,7 +1,7 @@
 import { FaPlusCircle, FaMinusCircle } from 'react-icons/fa'
 import { FaTrashCan } from 'react-icons/fa6'
 
-export function ShoppingItem ({name, quantity, price, picture, onIncrement, onDecrement}){
+export function ShoppingItem ({name, quantity, price, picture, onIncrement, onDecrement, onDelete}){
   return(
     <li className='flex flex-col md:flex-row gap-4 items-center md:justify-around border-2 border-slate-200 rounded-lg py-4 md:py-2 w-full md:px-6 mt-4'>
         <div className='flex-shrink-0 w-24 h-24 md:w-12 md:h-12 overflow-hidden rounded'>
@@ -22,7 +22,7 @@ export function ShoppingItem ({name, quantity, price, picture, onIncrement, onDe
         <FaPlusCircle  className='hover:text-red-700 text-xl' onClick={onIncrement} />
         <FaMinusCircle className='hover:text-red-700 text-xl' onClick={onDecrement} />
         </div>
-    <FaTrashCan className='hover:text-red-700 text-xl' />
+    <FaTrashCan className='hover:text-red-700 text-xl cursor-pointer' onClick={onDelete} />
     </li>
   )
 }
